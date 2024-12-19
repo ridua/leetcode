bf694be90605383c5a9732b1f4be05e43de1139f
Rishabbh A Dua <ridua@outlook.com>
2024-12-18 22:21:19 -0500

    problem 27

---
+++ 27_remove_element.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+  *a = *a ^ *b;
+  *b = *a ^ *b;
+  *a = *a ^ *b;
+}
+
+
+int removeElement(int* a, int n, int key) {
+
+  int i = 0;
+  int k = 0;
+
+  for (i = 0; i <n; i++) {
+      if (a[i] != key) {
+          a[k] = a[i];
+          k++;
+      }
+  }
+  return k;
+}
+
+
+int main()
+{
+  // int arr[] = {3,2,2,3};
+  // int arr[] = {3,3};
+  // int arr[] = {3};
+  int arr[] = {32,2};
+
+  int rv = removeElement(arr, sizeof(arr)/sizeof(arr[0]), 3);
+
+  printf("\n %d\n", rv);
+  return 0;
+}
